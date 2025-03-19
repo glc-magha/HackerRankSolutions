92cbf3c19dcb9b093f0a6134e80ab73fd618c0a7
90535 <glcgithub@gmail.com>
2025-03-19 15:33:29 +0300

    HackerrankSolution

--- 03_Question_Arit.Operator.py
+++ 03_Question_Arit.Operator.py
@@ -1,12 +1,14 @@
 """Task
-The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+The provided code stub reads two integers from STDIN,  and .
+ Add code to print three lines where:
 
 The first line contains the sum of the two numbers.
 The second line contains the difference of the two numbers (first - second).
 The third line contains the product of the two numbers.
 
 Görev
-Sağlanan kod parçası STDIN'den iki tam sayı okur ve . Üç satır yazdırmak için kod ekleyin:
+Sağlanan kod parçası STDIN'den iki tam sayı okur ve .
+ Üç satır yazdırmak için kod ekleyin:
 
 İlk satır iki sayının toplamını içerir.
 İkinci satır iki sayının farkını içerir (birinci - ikinci).
--- 04_Question_Bölme.py
+++ 04_Question_Bölme.py
@@ -1,6 +1,7 @@
 """The provided code stub reads two integers,  and , from STDIN.
 
-Add logic to print two lines. The first line should contain the result of integer division,  // .
+Add logic to print two lines.
+ The first line should contain the result of integer division,  // .
 The second line should contain the result of float division,  / .
 
 No rounding or formatting is necessary.
@@ -8,7 +9,9 @@ No rounding or formatting is necessary.
 
 Sağlanan kod parçası STDIN'den iki tam sayı ve okur.
 
-İki satır yazdırmak için mantık ekleyin. İlk satır tam sayı bölümünün sonucunu içermelidir, // . İkinci satır kayan nokta bölümünün sonucunu içermelidir, / .
+İki satır yazdırmak için mantık ekleyin.
+İlk satır tam sayı bölümünün sonucunu içermelidir, // .
+İkinci satır kayan nokta bölümünün sonucunu içermelidir, / .
 
 Yuvarlama veya biçimlendirme gerekli değildir."""
 # Read two integers from input
@@ -20,3 +23,4 @@ print(a // b)
 
 # Print float division result
 print(a / b)
+#done
