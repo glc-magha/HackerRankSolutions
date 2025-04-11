e20d0c9e707e6a678962499180e5f4b74555b734
90535 <glcgithub@gmail.com>
2025-04-12 00:48:27 +0300

    Hash Operation

--- 10_Hash_operation.py
+++ 10_Hash_operation.py
@@ -1,5 +1,5 @@
 
-Task
+"""Task
 Given an integer, , and  space-separated integers as input, create a tuple, , of those  integers. Then compute and print the result of .
 Note: hash() is one of the functions in the __builtins__ module, so it need not be imported.
 Input Format
@@ -21,4 +21,4 @@ Giriş Biçimi
 n = int(input())
 integer_list = map(int, input().split())
 t = tuple(integer_list)
-print(hash(t))
+print(hash(t))"""
